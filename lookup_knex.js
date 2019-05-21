d1efcd3f0860ac3f06053e12b69d4c968e4919c7
Jelena Grimshaw <41272899+jgrimshaw@users.noreply.github.com>
2019-05-21 17:59:20 -0600

    git issues, can't push file, copied the code here

--- lookup_knex.js
+++ lookup_knex.js
@@ -1,15 +1,27 @@
-const settings = require('./settings');
-
-
 const knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'development',
-    password : 'development',
-    database : 'test_db'
-  }
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'development',
+        password : 'development',
+        database : 'test_db'
+    }
 });
+
+console.log('this is redicilous!')
 
 const firstName = process.argv[2];
 
+knex.select('first_name', 'last_name', 'birthdate')
+   .from('famous_people')
+   .where('first_name', '=', firstName)
+   .asCallback(function(err, rows){
+    if (err) return console.error(err);
+    console.log('Searching ... ')
+    console.log(`Found ${rows.length} person(s) by the name of ${firstName}`)
+
+    rows.forEach(function(elm, index){
+      console.log(`- ${index + 1} ${elm.first_name} ${elm.last_name}, born '${elm.birthdate.toISOString().substr(0, 10)}'`)
+    })
+    knex.destroy();
+});
