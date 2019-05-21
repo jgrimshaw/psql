30f4d6b492953e1b02bb7eec714b2dda9b7f4294
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-21 21:34:13 +0000

    add lookup_knex.js

---
+++ lookup_knex.js
@@ -0,0 +1,14 @@
+const settings = require('./settings');
+
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'development',
+    password : 'development',
+    database : 'test_db'
+  }
+});
+
+const firstName = process.argv[2];
